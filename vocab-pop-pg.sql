fb848471f7bae63e4b060edd45fead5e69dfcef4
Sigfried Gold <sigfried@sigfried.org>
2016-12-27 11:04:20 -0500

    wip

--- vocab-pop-pg.sql
+++ vocab-pop-pg.sql
@@ -3,41 +3,165 @@ set session my.vars.cdm = :cdm; -- lets me use it as string in query (as per htt
 \set results results2
 set session my.vars.results = :results;
 
-drop table if exists :results.concept_id_occurrence;
+create or replace view :results.concept_cols as
+  select (table_schema||'.'||table_name)::regclass as tbl, 
+            table_name,
+            column_name::text as col
+  from information_schema.columns 
+  where
+        table_schema = current_setting('my.vars.cdm') 
+        and table_name not like '%concept%' 
+        and table_name != 'fact_relationship' -- at least for now
+        and (
+              column_name like '%concept_id%' or
+              column_name like '%source_value'
+            )
+    ;
 
+create or replace view :results.concept_cols_by_table as
+  with denest as
+  (
+    with col_grps as
+    (
+      with table_cols as
+      (
+        select table_name,
+                array_agg(col) as cols
+        from concept_cols cc
+        group by table_name
+      )
+      select  table_name,
+              (select array_agg(type_cols) from unnest(cols) as type_cols where type_cols like '%type%') as type_cols,
+              (select array_agg(source_cols) from unnest(cols) as source_cols where source_cols like '%source%' and source_cols not like '%source_value') as source_cols,
+              (select array_agg(source_value_cols) from unnest(cols) as source_value_cols where source_value_cols like '%source_value') as source_value_cols,
+              /*(select array_to_string(array_agg(other_cols), E'\n') from unnest(cols) as other_cols where other_cols not like '%type%') as other_cols_str,*/
+              (select array_agg(other_cols) from unnest(cols) as other_cols where other_cols not like '%type%' and other_cols not like '%source%') as target_cols
+              /*array_to_string(cols, E'\n') || E'\n' cols*/
+      from table_cols
+      group by table_name, cols
+    )
+    select table_name, 
+            unnest(target_cols) target_col, /*, cols*/
+            unnest(type_cols) type_col, 
+            unnest(source_cols) source_col, 
+            unnest(source_value_cols) source_value_col
+    from col_grps
+  )
+  select *
+  from denest
+  where type_col is not null
+    and source_col is not null
+    and target_col is not null
+    and (
+          target_col not like 'qualifier%' and
+          target_col not like 'value%' and
+          target_col not like 'modifier%' and
+          target_col not like 'operator%' and
+          target_col not like 'range%' and
+          target_col not like 'priority%' and
+          target_col not like 'dose_unit%' and
+          target_col not like 'route%' and
+          target_col not like 'unit%' and
+          target_col not like 'death_impute%'
+    )
+  ;
+
+
+drop table if exists :results.concept_id_occurrence;
 create table :results.concept_id_occurrence (
   --table_schema information_schema.sql_identifier,
+  schema text,
   table_name text,
-  column_name text,
-  concept_id integer,
+  target_column_name text,
+  type_column_name text,
+  source_column_name text,
+  source_value_column_name text,
+  target_concept_id integer,
+  type_concept_id integer,
+  source_concept_id integer,
+  source_value text,
   count bigint
 );
 
 CREATE OR REPLACE 
-  FUNCTION store_concept_id_counts(_qtbl regclass, _tbl, _col text, target_table regclass) 
+  FUNCTION store_concept_id_counts(
+              _schema text,
+              _tbl text,
+              _target_col text, 
+              _type_col text, 
+              _source_col text, 
+              _source_value_col text, 
+              target_schema text,
+              target_table text
+            ) 
   RETURNS TABLE(
+                  resultsSchema text,
+                  cdmSchema text,
                   table_name text,
-                  column_name text,
-                  concept_id integer,
+                  target_column_name text,
+                  type_column_name text,
+                  source_column_name text,
+                  source_value_column_name text,
+                  target_concept_id integer,
+                  type_concept_id integer,
+                  source_concept_id integer,
+                  source_value text,
                   count bigint
                 )
   AS
   $func$
+  declare sql text;
   BEGIN
-    RAISE NOTICE 'getting concept_ids for %.%', _tbl, _col;
-    EXECUTE format(
-      'INSERT INTO %s ' ||
+
+    RAISE NOTICE 'getting concept_ids for %', _tbl;
+    sql := format(
+      'INSERT INTO %s.%s ' ||
       'SELECT ''%s'' as table_name, ' ||
-      '       ''%s'' as column_name, ' ||
-      '       %s as concept_id, ' ||
+      '       ''%s'' as target_column_name, ' ||
+      '       ''%s'' as type_column_name, ' ||
+      '       ''%s'' as source_column_name, ' ||
+      '       ''%s'' as source_value_column_name, ' ||
+      '       %s as target_concept_id, ' ||
+      '       %s as type_concept_id, ' ||
+      '       %s as source_concept_id, ' ||
+      '       ''%s'' as source_value, ' ||
       '       count(*) as count ' ||
-      'from %s t ' ||
+      'from %s.%s t ' ||
       --'where %s > 0 ' ||
-      'group by 1,2,3'
-      , target_table, _tbl, _col, _col, _qtbl);
+      'group by 1,2,3,4,5,6,7,8,9',
+        target_schema, target_table, 
+        _tbl, 
+
+        _target_col, 
+        _type_col, 
+        _source_col, 
+        _source_value_col, 
+
+        _target_col, 
+        _type_col, 
+        _source_col, 
+        _source_value_col, 
+        
+        _schema, _tbl
+      );
+    RAISE NOTICE '%', sql;
+    EXECUTE sql;
   END;
   $func$ LANGUAGE plpgsql;
   
+select store_concept_id_counts(x.*) from
+  (select 'cdm2'::text, 
+          table_name::text,
+          target_col::text,
+          type_col::text,
+          source_col::text,
+          source_value_col::text,
+          current_setting('my.vars.results')::text, 
+          'concept_id_occurrence'::text
+    from concept_cols_by_table cct) x;
+
+
+
 select store_concept_id_counts(
           tbl, col,
           current_setting('my.vars.results')||'.concept_id_occurrence'
@@ -224,42 +348,84 @@ group by 1,2,3,4;
 
 
 /* should be view or materialzed view */
-drop table if exists :results.class_relations cascade;
-create table :results.class_relations as
+drop table if exists :results.class_relations_pre;
+create table :results.class_relations_pre as
 select
-        case when c1.vocabulary_id = c2.vocabulary_id then true else false end as same_vocab,
-        case when c1.concept_class_id = c2.concept_class_id then true else false end as same_class,
-
         r.relationship_id,
         r.relationship_name,
         r.is_hierarchical,
         r.defines_ancestry,
         r.reverse_relationship_id,
 
-          /*
-            thought i should include No Matching Concept indicator since concept_id
-            isn't included, but you can get it by testing vocab_1 = 'None'
-          */
+        
+        cr.concept_id_1,
         c1.domain_id as domain_id_1,
         c1.vocabulary_id as vocab_1,
         c1.concept_class_id as class_1,
         c1.standard_concept as sc_1,
-        case when c1.invalid_reason is null then false else true end as invalid_1,
+        c1.invalid_reason as invalid_1,
 
+        cr.concept_id_2,
         c2.domain_id as domain_id_2,
         c2.vocabulary_id as vocab_2,
         c2.concept_class_id as class_2,
         c2.standard_concept as sc_2,
-        case when c2.invalid_reason is null then false else true end as invalid_2,
-
-        count(distinct c1.concept_id) as c1_ids,
-        count(distinct c2.concept_id) as c2_ids,
-        count(*) c
+        c2.invalid_reason as invalid_2
 from :cdm.concept_relationship cr
 join :cdm.concept c1 on cr.concept_id_1 = c1.concept_id /* and c1.invalid_reason is null */
 join :cdm.concept c2 on cr.concept_id_2 = c2.concept_id /* and c2.invalid_reason is null */
 join :cdm.relationship r on cr.relationship_id = r.relationship_id
 where cr.invalid_reason is null
+;
+
+create index class_relations_pre_idx on :results.class_relations_pre (
+        relationship_id,
+        relationship_name,
+        is_hierarchical,
+        defines_ancestry,
+        reverse_relationship_id,
+        domain_id_1,
+        vocab_1,
+        class_1,
+        sc_1,
+        invalid_1,
+        domain_id_2,
+        vocab_2,
+        class_2,
+        sc_2,
+        invalid_2);
+
+drop table if exists :results.class_relations cascade;
+create table :results.class_relations as
+select
+        case when vocab_1 = vocab_2 then true else false end as same_vocab,
+        case when class_1 = class_2 then true else false end as same_class,
+
+        relationship_id,
+        relationship_name,
+        is_hierarchical,
+        defines_ancestry,
+        reverse_relationship_id,
+          /*
+            thought i should include No Matching Concept indicator since concept_id
+            isn't included, but you can get it by testing vocab_1 = 'None'
+          */
+        domain_id_1,
+        vocab_1,
+        class_1,
+        sc_1,
+        invalid_1,
+
+        domain_id_2,
+        vocab_2,
+        class_2,
+        sc_2,
+        invalid_2,
+
+        count(distinct concept_id_1) as c1_ids,
+        count(distinct concept_id_2) as c2_ids,
+        count(*) c
+from :results.class_relations_pre cr
 group by 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17
 ;
 /* where r.is_hierarchical = '1' and c1.vocabulary_id = c2.vocabulary_id */
