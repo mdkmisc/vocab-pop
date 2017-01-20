c4604d101db4d4077a54cfb256f51463a17577d6
Sigfried Gold <sigfried@sigfried.org>
2017-01-20 06:37:22 -0500

    wip, sql

--- vocab-pop-pg.sql
+++ vocab-pop-pg.sql
@@ -134,64 +134,6 @@ select store_concept_id_counts(
     from concept_cols cc) x;
 
 
-
-
-
-
-
-
-drop table if exists :results.record_counts;
-create table :results.record_counts as (
-  with ccounts as (
-    with concepts as (
-      select CAST(ancestor_concept_id as VARCHAR) ancestor_id, CAST(descendant_concept_id as VARCHAR) descendant_id
-      from cdm2.concept_ancestor ca
-    ), counts as (
-      select stratum_1 concept_id, max(count_value) agg_count_value
-      from results2.achilles_results
-      where analysis_id in (2, 4, 5, 201, 301, 401, 501, 505, 601, 701, 801, 901, 1001,1201,1801)
-      group by stratum_1
-      union
-      select stratum_2 as concept_id, sum(count_value) as agg_count_value
-      from results2.achilles_results
-      where analysis_id in (405, 605, 705, 805, 807, 1805, 1807)
-      group by stratum_2
-    )
-    select concepts.ancestor_id concept_id, 
-            coalesce(max(c1.agg_count_value),0) record_count, 
-            coalesce(sum(c2.agg_count_value),0) descendant_record_count
-    from concepts
-    left join counts c1 on concepts.ancestor_id = c1.concept_id
-    left join counts c2 on concepts.descendant_id = c2.concept_id
-    group by concepts.ancestor_id
-  )
-  select * from ccounts where record_count > 0 or descendant_record_count > 0
-);
-
-
-
-
-
-
-
-
-
-select store_concept_id_counts(
-          tbl, col,
-          current_setting('my.vars.results')||'.concept_id_occurrence'
-) from (
-  /* select (table_schema||'.'||table_name)::regclass as tbl, column_name::text as col */
-  select (table_schema||'.'||table_name)::regclass as tbl, 
-            table_name,
-            column_name::text as col
-  from information_schema.columns 
-  where table_schema = current_setting('my.vars.cdm') 
-    and table_name not like '%concept%' 
-    and table_name != 'fact_relationship' -- at least for now
-    and column_name like '%concept_id%'
-  --limit 3
-) cols;
-
 alter table :results.concept_id_occurrence add primary key (table_name, column_name, concept_id);
 create index cio_idx1 on :results.concept_id_occurrence (concept_id);
 
