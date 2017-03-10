dad69cfb2c67a89d975fd2a676729f66df4a8c58
Sigfried Gold <sigfried@sigfried.org>
2017-03-10 08:03:25 -0500

    made some fixes to one-way relationships before realizing I didn't need them

--- sql-scripts/descendants-based-on-concept_relationship.sql
+++ sql-scripts/descendants-based-on-concept_relationship.sql
@@ -1,32 +1,56 @@
-drop table if exists :results one_way_relationships;
+drop table if exists :results.one_way_relationships;
 create table :results.one_way_relationships as
-  select r.*
+  select distinct r.*
   from :cdm.relationship r
   join (
-      select r1.relationship_id rid_1, r1.reverse_relationship_id rrid_1, r2.relationship_id rid_2,r2.reverse_relationship_id rrid_2,
+      select r1.relationship_id rid_1, 
+              r1.reverse_relationship_id rrid_1, 
+              r2.relationship_id rid_2,
+              r2.reverse_relationship_id rrid_2,
               r1.defines_ancestry d1,
               r2.defines_ancestry d2,
               r1.is_hierarchical h1,
               r2.is_hierarchical h2,
+              /*
               case when r1.relationship_id < r2.relationship_id then r1.relationship_id else r2.relationship_id end relationship_id,
               case when r1.relationship_id < r2.relationship_id then r1.relationship_id else r2.relationship_name end relationship_name,
-              case when r1.relationship_id < r2.relationship_id then 'r1' else 'r2' end,
+              */
+              case 
+                    when r1.defines_ancestry::integer + r1.is_hierarchical::integer > r2.defines_ancestry::integer + r2.is_hierarchical::integer then r1.relationship_id
+                    when r1.defines_ancestry::integer + r1.is_hierarchical::integer < r2.defines_ancestry::integer + r2.is_hierarchical::integer then r2.relationship_id
+                    when r1.relationship_id like '%to%' and r2.relationship_id like '%from%' then r1.relationship_id
+                    when r1.relationship_id like '%from%' and r2.relationship_id like '%to%' then r2.relationship_id
+                    when r1.relationship_id < r2.relationship_id then r1.relationship_id
+                    when r2.relationship_id < r1.relationship_id then r2.relationship_id
+              end as relationship_id,
+              /*
+              case 
+                    when r1.defines_ancestry::integer + r1.is_hierarchical::integer > r2.defines_ancestry::integer + r2.is_hierarchical::integer then 'r1'
+                    when r1.relationship_id like '%to%' and r2.relationship_id like '%from%' then 'r1'
+                    when r1.defines_ancestry::integer + r1.is_hierarchical::integer = r2.defines_ancestry::integer + r2.is_hierarchical::integer then 'r1'
+                    else 'r2'
+              end as from_which,
+              */
               r1.relationship_name,
               r2.relationship_name
       from :cdm.relationship r1
       left join :cdm.relationship r2 on r1.relationship_id = r2.reverse_relationship_id or  r2.relationship_id = r1.reverse_relationship_id
+      /*
       where
             (r1.defines_ancestry::integer + r1.is_hierarchical::integer = r2.defines_ancestry::integer + r2.is_hierarchical::integer and r1.relationship_id < r2.relationship_id) or
             (r1.defines_ancestry::integer + r1.is_hierarchical::integer > r2.defines_ancestry::integer + r2.is_hierarchical::integer and r1.relationship_id < r2.relationship_id) or
             (r1.defines_ancestry::integer + r1.is_hierarchical::integer < r2.defines_ancestry::integer + r2.is_hierarchical::integer and r1.relationship_id > r2.relationship_id)
+      */
       order by 9,10
     ) one_way on r.relationship_id = one_way.relationship_id
+    order by 4
   ;
 create index onewayrelidx on :results.one_way_relationships (relationship_id);
 -- trying to understand non-standard concepts:
 
 -- realizing that relationship_name does have vital information for understanding relationships
 
+/*
 select * from :cdm.concept where concept_id = 44824072;
 
 select  distinct
@@ -51,7 +75,6 @@ and   cr.invalid_reason is null
 group by 1,2,3,4,5,6,7
 order by 3, sc, domain_id, vocabulary_id;
 
-/*
 
 select c.concept_id,c.concept_name,c.domain_id,c.vocabulary_id,
         count(cr.concept_id_1) crs,count(distinct cr.concept_id_1) id1s, count(distinct cr.concept_id_2) id2s,
